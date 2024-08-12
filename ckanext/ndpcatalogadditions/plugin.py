7d55302ca2f4c25d8b9ea3e10a502099e0159585
Kai Lin <kaiucsd@gmail.com>
2024-08-12 07:51:31 -0700

    Update plugin.py

--- ckanext/ndpcatalogadditions/plugin.py
+++ ckanext/ndpcatalogadditions/plugin.py
@@ -4,13 +4,8 @@ from flask import Blueprint
 from ckanext.ndp.controller import create_package, update_package, delete_package, purge_package, list_my_packages, approve_package, reject_package
 
 
-def jupyterhub_endpoint():
-    value = toolkit.config.get('ckanext.ndp.jupyterhub_endpoint')
-    return value
-
 class NdpcatalogadditionsPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.ITemplateHelpers)
     plugins.implements(plugins.IBlueprint)    
     
     # IConfigurer
@@ -18,11 +13,7 @@ class NdpcatalogadditionsPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "ndp")
-
-    # ITemplateHelpers
-    def get_helpers(self):
-        return {'ndp_jupyterhub_endpoint': jupyterhub_endpoint}
-
+        
     def get_blueprint(self):
         blueprint = Blueprint(self.name, self.__module__)
 
